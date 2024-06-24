1b2cc8a14b34ae644de33563d159f83420d9ae49
DiniJNS <jonas.dinilethu@gmail.com>
2024-06-24 15:54:29 +0200

    added paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { ViewProductsComponent } from './view-products/view-products.component';
+import { ViewOneProductComponent } from './view-one-product/view-one-product.component';
+import { ViewOrdersComponent } from './view-orders/view-orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'admin', component: AdminDashboardComponent},
+  {path : 'addproduct', component: AddproductComponent},
+  {path : 'oneproduct', component: ViewOneProductComponent},
+  {path : 'view products', component: ViewProductsComponent},
+  {path : 'Orders', component: ViewOrdersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
