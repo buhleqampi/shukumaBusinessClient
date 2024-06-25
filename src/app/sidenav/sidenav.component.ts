2d1f66f55097489f39547ebd8df5710c72b96834
DiniJNS <jonas.dinilethu@gmail.com>
2024-06-25 14:19:47 +0200

    removed comments

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -14,51 +14,6 @@ export class SidenavComponent {
 isSidebarOpen: any;
 toggleSidebar() {
 throw new Error('Method not implemented.');
-<<<<<<< HEAD
-=======
-}
- collapsed = false;
- navData = [
-  {
-    label: 'Home',
-    routerLink: '/',
-    icon: 'fas fa-home'
-  },
-  {
-    label: 'Stats',
-    routerLink: '/admin',
-    icon: 'fas fa-chart-bar'
-  },
-  {
-   label: 'Chats',
-   RouterLink: '',
-   icon:'fas fa-comment'
-  },
-  {
-   label:'Settings',
-   RouterLink: '',
-   icon:'fas fa-cog'
-  },
-
-  {
-   label: 'LogOut',
-   RouterLink:'',
-   icon: 'fas fa-sign-out'
-  },
-  // Add more nav items as needed
-];
-
- @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
-  screenwidth!: number;
- toggleCollapse():void{
-  this.collapsed = !this.collapsed;
-  this.onToggleSideNav.emit({collapsed: this.collapsed, screenwidth: this.screenwidth})
- }
-
- closeSidenav() :void{
-  this.collapsed = false;
- }
->>>>>>> feature/Dini
 }
  collapsed = false;
  navData = [
