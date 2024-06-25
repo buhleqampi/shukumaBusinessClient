dd2f73fbbe2a6cba3ae333750b63cfc7e72eddeb
DiniJNS <jonas.dinilethu@gmail.com>
2024-06-25 13:06:17 +0200

    sidenav ts

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
-import { navbarData } from './navData';
+import { Component, Output, EventEmitter } from '@angular/core';
 
+interface SideNavToggle {
+  screenwidth:number;
+  collapsed : boolean;
+}
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
- collapsed = false;
- navData = navbarData;
+isSidebarOpen: any;
+toggleSidebar() {
+throw new Error('Method not implemented.');
 }
+ collapsed = false;
+ navData = [
+  {
+    label: 'Home',
+    routerLink: '/',
+    icon: 'fas fa-home'
+  },
+  {
+    label: 'Chart',
+    routerLink: '/admin',
+    icon: 'fas fa-chart-bar'
+  },
+  {
+   label: 'Stats'
+  },
+  // Add more nav items as needed
+];
+
+ @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+  screenwidth!: number;
+ toggleCollapse():void{
+  this.collapsed = !this.collapsed;
+  this.onToggleSideNav.emit({collapsed: this.collapsed, screenwidth: this.screenwidth})
+ }
+
+ closeSidenav() :void{
+  this.collapsed = false;
+ }
+}
+
