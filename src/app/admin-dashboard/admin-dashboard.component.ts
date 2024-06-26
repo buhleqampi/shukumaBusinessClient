94b559721942f5b34db3c5cb182bf494c70e9fb4
DiniJNS <jonas.dinilethu@gmail.com>
2024-06-26 11:16:18 +0200

    changed dashboard

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+  salesValue: number = 0;
+  visitorsValue: number = 0;
+  ordersValue: number = 0;
 
+  ngOnInit() {
+    this.updateData();
+    setInterval(() => {
+      this.updateData();
+    }, 2000);
+  }
+
+  updateData() {
+    this.salesValue = Math.floor(Math.random() * 10000);
+    this.visitorsValue = Math.floor(Math.random() * 1000);
+    this.ordersValue = Math.floor(Math.random() * 100);
+  }
 }
