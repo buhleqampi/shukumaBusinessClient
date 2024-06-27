3552020273897c3aaff90dc665a7ec7701d688d7
buhleqampi <buhle3857@gmail.com>
2024-06-27 13:18:23 +0200

    viewproduct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
+// import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +38,7 @@ import { BusinessprofileComponent } from './businessprofile/businessprofile.comp
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgxChartsModule
+    // NgxChartsModule
   ],
   providers: [
     provideClientHydration()
--- src/app/view-one-product/view-one-product.component.ts
+++ src/app/view-one-product/view-one-product.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../interface/product';
+import { products } from '../../../mock-products';
 
 @Component({
   selector: 'app-view-one-product',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './view-one-product.component.css'
 })
 export class ViewOneProductComponent {
+  product: Product | undefined;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.product = products.find(p => p.id );
+    }
+  }
 }
