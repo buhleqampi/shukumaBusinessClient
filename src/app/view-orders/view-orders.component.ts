f3884ed786e9042175c3b0c836296ea2630120a8
buhleqampi <buhle3857@gmail.com>
2024-06-28 16:38:15 +0200

    changes

--- src/app/view-orders/view-orders.component.ts
+++ src/app/view-orders/view-orders.component.ts
@@ -1,35 +1,12 @@
+import { Component } from '@angular/core';
 import { orders } from './../../../mock-data';
-import { Component, OnInit } from '@angular/core';
-import { Order } from '../interface/order';
-import { ActivatedRoute } from '@angular/router';
-
 
 @Component({
   selector: 'app-view-orders',
   templateUrl: './view-orders.component.html',
   styleUrl: './view-orders.component.css'
 })
-export class ViewOrdersComponent implements OnInit {
 
-  orders: any;
-
-  constructor(private route: ActivatedRoute) { }
-
-  ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.orders = orders.find(p => p.id );
-    }}}
-//   }
-// import { Component } from '@angular/core';
-// import { orders } from './../../../mock-data';
-
-// @Component({
-//   selector: 'app-view-orders',
-//   templateUrl: './view-orders.component.html',
-//   styleUrl: './view-orders.component.css'
-// })
-
-// export class ViewOrdersComponent  {
-// }
+export class ViewOrdersComponent  {
+}
 
